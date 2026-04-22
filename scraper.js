a171f8d16388aa02f661e1ff5f002d5d6fff375f
tuvalmatan <104898614+tuvalmatan@users.noreply.github.com>
2026-04-22 22:22:48 +0300

    Update scraper.js

--- scraper.js
+++ scraper.js
@@ -6,15 +6,31 @@ const config = require('./config.json');
 const getYad2Response = async (url) => {
     const requestOptions = {
         method: 'GET',
-        redirect: 'follow'
+        redirect: 'follow',
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+            'Accept-Language': 'he-IL,he;q=0.9,en-US;q=0.8,en;q=0.7',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Upgrade-Insecure-Requests': '1',
+            'Cache-Control': 'max-age=0',
+            'sec-ch-ua': '"Chromium";v="124", "Google Chrome";v="124"',
+            'sec-ch-ua-mobile': '?0',
+            'sec-ch-ua-platform': '"Windows"',
+            'sec-fetch-dest': 'document',
+            'sec-fetch-mode': 'navigate',
+            'sec-fetch-site': 'none',
+            'Referer': 'https://www.yad2.co.il/',
+        }
     };
     try {
-        const res = await fetch(url, requestOptions)
-        return await res.text()
+        const res = await fetch(url, requestOptions);
+        return await res.text();
     } catch (err) {
-        console.log(err)
+        console.log(err);
     }
-}
+};
 
 const scrapeItemsAndExtractImgUrls = async (url) => {
     const yad2Html = await getYad2Response(url);
@@ -22,7 +38,7 @@ const scrapeItemsAndExtractImgUrls = async (url) => {
         throw new Error("Could not get Yad2 response");
     }
     const $ = cheerio.load(yad2Html);
-    const title = $("title")
+    const title = $("title");
     const titleText = title.first().text();
     if (titleText === "ShieldSquare Captcha") {
         throw new Error("Bot detection");
@@ -31,15 +47,15 @@ const scrapeItemsAndExtractImgUrls = async (url) => {
     if (!$feedItems) {
         throw new Error("Could not find feed items");
     }
-    const imageUrls = []
+    const imageUrls = [];
     $feedItems.each((_, elm) => {
         const imgSrc = $(elm).find("img").attr('src');
         if (imgSrc) {
-            imageUrls.push(imgSrc)
+            imageUrls.push(imgSrc);
         }
-    })
+    });
     return imageUrls;
-}
+};
 
 const checkIfHasNewItem = async (imgUrls, topic) => {
     const filePath = `./data/${topic}.json`;
@@ -74,23 +90,23 @@ const checkIfHasNewItem = async (imgUrls, topic) => {
         await createPushFlagForWorkflow();
     }
     return newItems;
-}
+};
 
 const createPushFlagForWorkflow = () => {
-    fs.writeFileSync("push_me", "")
-}
+    fs.writeFileSync("push_me", "");
+};
 
 const scrape = async (topic, url) => {
     const apiToken = process.env.API_TOKEN || config.telegramApiToken;
     const chatId = process.env.CHAT_ID || config.chatId;
-    const telenode = new Telenode({apiToken})
+    const telenode = new Telenode({ apiToken });
     try {
-        await telenode.sendTextMessage(`Starting scanning ${topic} on link:\n${url}`, chatId)
+        await telenode.sendTextMessage(`Starting scanning ${topic} on link:\n${url}`, chatId);
         const scrapeImgResults = await scrapeItemsAndExtractImgUrls(url);
         const newItems = await checkIfHasNewItem(scrapeImgResults, topic);
         if (newItems.length > 0) {
             const newItemsJoined = newItems.join("\n----------\n");
-            const msg = `${newItems.length} new items:\n${newItemsJoined}`
+            const msg = `${newItems.length} new items:\n${newItemsJoined}`;
             await telenode.sendTextMessage(msg, chatId);
         } else {
             await telenode.sendTextMessage("No new items were added", chatId);
@@ -98,12 +114,12 @@ const scrape = async (topic, url) => {
     } catch (e) {
         let errMsg = e?.message || "";
         if (errMsg) {
-            errMsg = `Error: ${errMsg}`
+            errMsg = `Error: ${errMsg}`;
         }
-        await telenode.sendTextMessage(`Scan workflow failed... 😥\n${errMsg}`, chatId)
-        throw new Error(e)
+        await telenode.sendTextMessage(`Scan workflow failed... 😥\n${errMsg}`, chatId);
+        throw new Error(e);
     }
-}
+};
 
 const program = async () => {
     await Promise.all(config.projects.filter(project => {
@@ -112,8 +128,8 @@ const program = async () => {
         }
         return !project.disabled;
     }).map(async project => {
-        await scrape(project.topic, project.url)
-    }))
+        await scrape(project.topic, project.url);
+    }));
 };
 
 program();
